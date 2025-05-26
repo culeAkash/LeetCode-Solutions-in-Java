e4837c902591e071d89147bbd1a0849e276ed5a2
Akash Jaiswal <90236346+culeAkash@users.noreply.github.com>
2025-05-26 21:23:36 +0530

    Time: 323 ms (5.11%), Space: 115.8 MB (67.3%) - LeetHub

---
+++ 1986-largest-color-value-in-a-directed-graph/1986-largest-color-value-in-a-directed-graph.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int largestPathValue(String colors, int[][] edges) {
+        int N = colors.length();
+        List<List<Integer>> graph = new ArrayList<>();
+        int[] indegree = new int[N];
+
+        for(int i=0;i<N;i++)
+            graph.add(new ArrayList<>());
+
+        for(int[] edge : edges){
+            int u = edge[0];
+            int v = edge[1];
+            graph.get(u).add(v);
+            indegree[v]++;
+        }
+
+        Map<Integer,int[]> dp = new HashMap<>();
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0;i<N;i++){
+            dp.put(i,new int[26]);
+            if(indegree[i]==0)
+                q.offer(i);
+            int color = colors.charAt(i) - 'a';
+            dp.get(i)[color] = 1;
+        }
+
+
+        int visited = 0;
+        int maxValue = 0;
+
+
+        while(!q.isEmpty()){
+            int node = q.poll();
+            visited++;
+
+            for(int it : graph.get(node)){
+                for(int c=0;c<26;c++){
+                    int inc = colors.charAt(it) -'a' == c ? 1 : 0;
+                    dp.get(it)[c] = Math.max(dp.get(it)[c],dp.get(node)[c] + inc);
+                }
+
+                if(--indegree[it]==0){
+                    q.offer(it);
+                }
+            }
+
+            for(int val : dp.get(node))
+                maxValue = Math.max(val,maxValue);
+        }
+
+        return visited==N ? maxValue : -1;
+    }
+}
