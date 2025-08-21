8ad21b6009d4a2a5c16d2a36da730c1eea093b6e
Akash Jaiswal <90236346+culeAkash@users.noreply.github.com>
2025-08-21 23:23:09 +0530

    Time: 17 ms (21.23%), Space: 45.1 MB (94.86%) - LeetHub

--- 1628-count-submatrices-with-all-ones/1628-count-submatrices-with-all-ones.java
+++ 1628-count-submatrices-with-all-ones/1628-count-submatrices-with-all-ones.java
@@ -1,25 +1,30 @@
 class Solution {
     public int numSubmat(int[][] mat) {
-        int m = mat.length;
         int n = mat[0].length;
-        int[][] row = new int[m][n];
-        
+        int[] heights = new int[n];
         int res = 0;
+        for(int[] row : mat){
+            for(int i=0;i<n;i++){
+                heights[i] = row[i] == 0 ? 0 : heights[i] + 1;
+            }
 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(j==0){
-                    row[i][j] = mat[i][j];
-                }else{
-                    row[i][j] = mat[i][j] == 0 ? 0 : row[i][j-1] + 1;
+            Stack<int[]> st = new Stack<>();
+            st.push(new int[]{-1,0,-1});
+
+            for(int j=0;j<row.length;j++){
+                int h = heights[j];
+
+                while(st.peek()[2] >= h){
+                    st.pop();
                 }
-                int cur = row[i][j];
-                for(int k=i;k>=0;k--){
-                    cur = Math.min(cur,row[k][j]);
-                    if(cur==0)
-                        break;
-                    res += cur;
-                }
+
+                int[] top = st.peek();
+                int prev = top[1];
+                int prevIndex = top[0];
+                
+                int cur = (j - prevIndex) * h + prev;
+                res += cur;
+                st.push(new int[]{j,cur,h});
             }
         }
         return res;
