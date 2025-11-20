5ad81996581432314f60de82e56579be0dddd3c9
Akash Jaiswal <90236346+culeAkash@users.noreply.github.com>
2025-11-20 22:46:41 +0530

    Time: 10 ms (57.14%), Space: 47.6 MB (11.56%) - LeetHub

--- 0759-set-intersection-size-at-least-two/0759-set-intersection-size-at-least-two.java
+++ 0759-set-intersection-size-at-least-two/0759-set-intersection-size-at-least-two.java
@@ -2,32 +2,23 @@ class Solution {
     public int intersectionSizeTwo(int[][] intervals) {
         Arrays.sort(intervals,(a,b)->a[1]==b[1] ? b[0] - a[0] : a[1] - b[1]);
 
-        List<Integer> nums = new ArrayList<>();
         int ans = 0;
+        int p1=-1,p2=-1;
 
         for(int[] interval : intervals){
-
             int start = interval[0],end = interval[1];
-            int count = 0;
-            for(int index = nums.size() - 1;index>=0;index--){
-                if(nums.get(index)>=start && nums.get(index)<=end){
-                    count++;
-                    if(count==2)break;
-                }
-            }
 
-            if(count==0){
-                ans += 2;
-                nums.add(end);
-                nums.add(end-1);
-            }else if(count==1){
-                ans += 1;
-                nums.add(end);
+            if(start > p2){
+                p2 = interval[1];
+                p1 = interval[1] - 1;
+                ans +=2;
+            }else if(start > p1){
+                p1 = p2;
+                p2 = interval[1];
+                ans++;
             }
         }
 
         return ans;
     }
-
-
 }
