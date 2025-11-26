26f63686ad957d621073612b73e82d56154beea5
Akash Jaiswal <90236346+culeAkash@users.noreply.github.com>
2025-11-26 07:41:57 +0530

    Time: 85 ms (45.04%), Space: 98.6 MB (17.56%) - LeetHub

--- 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.java
+++ 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.java
@@ -1,15 +1,41 @@
 class Solution {
     static int MOD = 1000000007;
-    public int numberOfPaths(int[][] grid, int k) {
+    public int numberOfPaths(int[][] grid, int div) {
         int M = grid.length;
         int N = grid[0].length;
-        int[][][] dp = new int[M][N][k];
-        for(int[][] d : dp){
-            for(int[] p : d){
-                Arrays.fill(p,-1);
+        int[][][] dp = new int[M+1][N+1][div];
+
+
+        for(int row=M-1;row>=0;row--){
+            for(int col=N-1;col>=0;col--){
+                for(int k=div-1;k>=0;k--){
+                    int newRem = (k+grid[row][col]) % div;
+
+                    if(row==M-1 && col==N-1){
+                        dp[row][col][k] = (newRem==0) ? 1 : 0;
+                    }else{
+                        int bottom = dp[row+1][col][newRem] % MOD;
+                    
+                    int right = dp[row][col+1][newRem] % MOD;
+                    
+
+                    dp[row][col][k] = (bottom + right) % MOD;
+                    }  
+                }
             }
         }
-        return helper(0,0,0,grid,M,N,k,dp);
+        // System.out.println(dp[M][N][0] + " " + M + " " + N);
+        // for(int d[][] : dp){
+        //     for(int[] p : d){
+        //         for(int a : p){
+        //             System.out.print(a + " ");
+        //         }
+        //         System.out.println();
+        //     }
+        //     System.out.println();
+        // }
+
+        return dp[0][0][0];
     }
 
     int helper(int row,int col,int rem,int[][] grid,int M,int N,int k,int[][][] dp){
