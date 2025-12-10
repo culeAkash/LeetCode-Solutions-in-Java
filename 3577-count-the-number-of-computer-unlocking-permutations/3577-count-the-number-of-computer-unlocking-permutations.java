b0297dc72a2eae93a5d3e196e0df59526283c0cc
Akash Jaiswal <90236346+culeAkash@users.noreply.github.com>
2025-12-10 08:25:29 +0530

    Time: 2 ms (74.67%), Space: 92 MB (26.67%) - LeetHub

---
+++ 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.java
@@ -0,0 +1,16 @@
+class Solution {
+    int MOD = 1000000007;
+    public int countPermutations(int[] complexity) {
+        long ans = 1L;
+        int min = complexity[0];
+        for(int index = 1;index < complexity.length;index++){
+            if(complexity[index] <= min) return 0;
+
+            min = Math.min(complexity[index],min);
+
+            ans = (ans * 1L * index) % MOD;
+        }
+
+        return (int)ans;
+    }
+}
