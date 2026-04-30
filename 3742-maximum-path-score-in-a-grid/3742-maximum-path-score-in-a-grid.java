e8b3fc950c60107e34ca7bd017d5890dda1c2376
Akash Jaiswal <90236346+culeAkash@users.noreply.github.com>
2026-04-30 08:35:48 +0530

    Time: 349 ms (43.55%), Space: 113.6 MB (29.03%) - LeetHub

--- 3742-maximum-path-score-in-a-grid/3742-maximum-path-score-in-a-grid.java
+++ 3742-maximum-path-score-in-a-grid/3742-maximum-path-score-in-a-grid.java
@@ -3,12 +3,37 @@ class Solution {
     public int maxPathScore(int[][] grid, int k) {
         int N = grid.length;
         int M = grid[0].length;
-        int[][][] dp = new int[N][M][k+1];
-        for(int [][] d : dp){
-            for(int[] p : d)
-                Arrays.fill(p,-1);
+        int[][][] dp = new int[N+1][M+1][k+1];
+        Arrays.fill(dp[N][M],MIN);
+
+        for(int row=N-1;row>=0;row--){
+            for(int col=M-1;col>=0;col--){
+                for(int c=0;c<=k;c++){
+                    int cost = grid[row][col] == 0 ? 0 : 1; 
+                    if(row==N-1 && col==M-1 && c - cost >= 0){
+                        dp[row][col][c] = grid[row][col];
+                        continue;
+                    }
+                    int right = MIN;
+                    if(row+1<N && c-cost>=0){
+                        right = grid[row][col] + dp[row+1][col][c-cost];
+                    }
+                    int bottom = MIN;
+                    if(col+1 < M && c-cost>=0){
+                        bottom = grid[row][col] + dp[row][col+1][c-cost];
+                    }
+
+                    dp[row][col][c] = Math.max(bottom,right);
+                }
+            }
         }
-        int res = helper(0,0,N,M,grid,k,dp);
+
+        int res = MIN;
+
+        for(int c=0;c<=k;c++){
+            res = Math.max(dp[0][0][c],res);
+        }
+    
         return  res < 0 ? -1 : res;
     }
 
@@ -18,15 +43,11 @@ class Solution {
         if(dp[row][col][k]!=-1)return dp[row][col][k];
 
         int cost = grid[row][col] == 0 ? 0 : 1;
-        int K = k;
-        k = k - cost;
-        if(row==N-1 && col==M-1 && k>=0)return grid[row][col];
+        if(row==N-1 && col==M-1 && k>=cost)return grid[row][col];
 
-        
+        int right = grid[row][col] + helper(row,col+1,N,M,grid,k-cost,dp);
+        int bottom = grid[row][col] + helper(row+1,col,N,M,grid,k-cost,dp);
 
-        int right = grid[row][col] + helper(row,col+1,N,M,grid,k,dp);
-        int bottom = grid[row][col] + helper(row+1,col,N,M,grid,k,dp);
-
-        return dp[row][col][K] = Math.max(right,bottom);
+        return dp[row][col][k] = Math.max(right,bottom);
     }
 }
